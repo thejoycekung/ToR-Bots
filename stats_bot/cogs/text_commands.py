e1d95a89c19528b98f55a6c069dd283fa88bdf6a
DavidArchibald <David@robarchibald.com>
2019-07-20 17:32:44 -0700

    fix witty as well

--- stats_bot/cogs/text_commands.py
+++ stats_bot/cogs/text_commands.py
@@ -489,9 +489,11 @@ class TextCommands(commands.Cog):
                 "that's just... so many transcriptions!"
             )
         else:
-            progress = f"`{witty_overflowed_progress}` - I'm running out of witty "
-            "things to convey that this is really impressive and really concerning... "
-            f"Seriously you've done {transcription_count} transcriptions in 24 hours."
+            progress = (
+                f"`{witty_overflowed_progress}` - I'm running out of witty "
+                "things to convey that this is really impressive and really concerning... "
+                f"Seriously you've done {transcription_count} transcriptions in 24 hours."
+            )
 
         await ctx.send(progress)
 
