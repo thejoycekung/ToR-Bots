d54f2ba4c7cbcbe5b3faa1576741da5e4681bd84
Tim Jentzsch <tim-jentzsch@gmx.de>
2021-06-07 15:34:17 +0200

    Move plot color definitions to init file

--- stats_bot/helpers/__init__.py
+++ stats_bot/helpers/__init__.py
@@ -1,4 +1,24 @@
+import matplotlib.pyplot as plot
 from .functions import get_redditor_name, add_user
 from .database_reader import delete_transcriber,fetch_transcribers
 
 __all__ = ["get_redditor_name", "add_user", "delete_transcriber", "fetch_transcribers"]
+
+# Colors to use in the plots
+background_color = "#4e6186"
+text_color = "white"
+line_color = "gray"
+
+# Global settings for the plots
+plot.rcParams['figure.facecolor'] = background_color
+plot.rcParams['axes.facecolor'] = background_color
+plot.rcParams['axes.labelcolor'] = text_color
+plot.rcParams['axes.edgecolor'] = line_color
+plot.rcParams['text.color'] = text_color
+plot.rcParams['xtick.color'] = line_color
+plot.rcParams['xtick.labelcolor'] = text_color
+plot.rcParams['ytick.color'] = line_color
+plot.rcParams['ytick.labelcolor'] = text_color
+plot.rcParams['grid.color'] = line_color
+plot.rcParams['grid.alpha'] = 0.8
+plot.rcParams["figure.dpi"] = 200.0
